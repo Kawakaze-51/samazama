96ec1efb107da126905c846cedb91e5838eb11a3
K.E.D <84723144+Kawakaze-51@users.noreply.github.com>
2024-02-15 10:18:20 +0800

    Update Blue_Archive.py

--- Blue_Archive.py
+++ Blue_Archive.py
@@ -66,6 +66,8 @@ APPCOMMAND_VOLUME_MAX = 0x0a
 APPCOMMAND_VOLUME_MIN = 0x09
 win32api.SendMessage(-1, WM_APPCOMMAND, 0x30292, APPCOMMAND_VOLUME_MAX * 0x10000)
 
+webbrowser.open('https://www.bilibili.com/video/BV1xC4y1k7an/?spm_id_from=333.999.0.0&vd_source=f9b17b48e7f7cc5240a76a93bedc349e')
+time.sleep(3)
 webbrowser.open('https://bluearchive-cn.com/')
 time.sleep(4)
 get_resolution()
